f0782660e02698f69d05074ce16d8f1ae64aab50
e-1-e <155574206+e-1-e@users.noreply.github.com>
2024-09-11 20:36:19 -0400

    9 11 24 (3)

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function populateUserTable() {
         buttonContainer.appendChild(howDoesThisWork);
 
         howDoesThisWork.addEventListener('click', ()=>{
-            appendSheet(keyCell.textContent, valueCell.textContent);
+            appendSheet({value: keyCell.textContent}, valueCell.textContent);
         });
 
         tableBody.appendChild(row);
